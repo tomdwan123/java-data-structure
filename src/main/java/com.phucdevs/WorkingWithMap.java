f35752358b9ba561e9f3d335e490eddb5a08840e
phuc.letan <phuc.letan@messagemedia.com>
2023-04-14 19:12:02 +0700

    ✨Working with Map

--- src/main/java/com.phucdevs/WorkingWithMap.java
+++ src/main/java/com.phucdevs/WorkingWithMap.java
@@ -2,11 +2,22 @@ package com.phucdevs;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class WorkingWithMap {
 
     public static void main(String[] args) {
 
+        maps1();
+
+        Map<Person, Diamond> map = new HashMap<>();
+        map.put(new Person("Tony"), new Diamond("Diamond Gold"));
+        map.put(new Person("Jate"), new Diamond("Diamond Silver"));
+
+        System.out.println(map.get( new Person("Tony") ));
+    }
+
+    private static void maps1() {
         Map<Integer, Person> map = new HashMap<>();
         map.put(1, new Person("Alex"));
         map.put(2, new Person("Ali"));
@@ -33,5 +44,27 @@ public class WorkingWithMap {
         System.out.println(map.values());
     }
 
-    record Person(String name){}
+    static class Person {
+
+        String name;
+
+        public Person(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Person person = (Person) o;
+            return Objects.equals(name, person.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
+    }
+
+    record Diamond(String name){}
 }
